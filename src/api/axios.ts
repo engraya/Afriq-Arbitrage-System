89951f29422159b19206ad6917438ccacbb0148b
Engraya <ahmadyakubuaya@gmail.com>
2025-02-07 11:09:32 +0100

    Fix Errors

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,9 +2,8 @@ import axios from "axios";
 
 export const apiClient = axios.create({
   baseURL: "https://limiance-token-sale.onrender.com/api/v1",
-  // // headers: {
-  // //   "Content-Type": "application/json",
-  // //   "Accept": "application/json",
-  // }
-  // withCredentials: true, // Include credentials if needed
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  }
 });
